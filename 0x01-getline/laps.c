dcaa7362ffab03d875a8978b310d39749fa1e025
evertcolombia <evertescalante@gmail.com>
2021-04-25 23:30:13 +0000

    add sorting quick sort

--- 0x01-getline/laps.c
+++ 0x01-getline/laps.c
@@ -1,5 +1,9 @@
 #include "laps.h"
 
+void keep_sort(int (*array)[2], ssize_t low, ssize_t high, size_t size);
+ssize_t partition(int (*array)[2], ssize_t low, ssize_t high);
+void do_swap(int (*array)[2], int pos1, int pos2);
+
 void print_race_state(int (*cars)[2], unsigned int size)
 {
 	unsigned int i = 0;
@@ -11,6 +15,101 @@ void print_race_state(int (*cars)[2], unsigned int size)
 		cars++;
 	}
 }
+
+/**
+ * quick_sort -sorting algoritmh
+ * @array: list array
+ * @size: size array
+ *
+ * Return: none
+ */
+void quick_sort(int (*array)[2], size_t size)
+{
+	unsigned int low, high;
+
+	if (size < 2 || !array)
+		return;
+	low = 0, high = size - 1;
+	keep_sort(array, low, high, size);
+}
+
+/**
+ * keep_sort - keep sorting the array
+ * @array: list array
+ * @low: low pont of the array
+ * @high: high part of the array
+ * @size: size array
+ *
+ * Return: None
+ */
+
+void keep_sort(int (*array)[2], ssize_t low, ssize_t high, size_t size)
+{
+	ssize_t pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, low, high);
+		keep_sort(array, low, pivot - 1, size);
+		keep_sort(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * partition - partition the array
+ * @array: list array
+ * @low: low part of the array
+ * @high: high part of the array
+ * @size: size array
+ *
+ * Return: pivot point in the partitioned array
+ */
+ssize_t partition(int (*array)[2], ssize_t low, ssize_t high)
+{
+	int pivot;
+	ssize_t i, j;
+
+	pivot = array[high][0];
+	i = (low - 1);
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j][0] < pivot)
+		{
+			i++;
+			if (i != j)
+				 do_swap(array, i, j);
+		}
+	}
+
+	if (array[i + 1][0] > array[high][0])
+	{
+		do_swap(array, (i + 1), high);
+	}
+	return(i + 1);
+	
+}
+
+/**
+ * do_swap - change  positions in array
+ * @array: array list
+ * @pos1: position to change
+ * @pos2: position to change
+ *
+ * Return: array pointer
+ */
+void do_swap(int (*array)[2], int pos1, int pos2)
+{
+	int temp[2];
+
+	temp[0] = array[pos1][0];
+	temp[1] = array[pos1][1];
+	array[pos1][0] = array[pos2][0];
+	array[pos1][1] = array[pos2][1];
+	array[pos2][0] = temp[0];
+	array[pos2][1] = temp[1];
+}
+
 
 void race_state(int *id, size_t size){
 
@@ -46,5 +145,6 @@ void race_state(int *id, size_t size){
 			}
 		}
 	}
+	quick_sort(cars, count);
 	print_race_state(cars, count);
 }
